78c7dfb40e11da73b1135fa561007f20fe74cd23
Tyrone Serapio <tyroneserapio@gmail.com>
2026-09-15 21:23:14 -0400

    added hw2 link

--- src/content.js
+++ src/content.js
@@ -95,12 +95,15 @@ export const sections = [
       columns: ['Homework', 'Out', 'Due', 'Code Review Sign Up'],
       links: {
         'Homework 1: Uninformed Search': 'https://hackmd.io/@cs410/SkUv4EDuMx',
+        'Homework 2: Informed Search': 'https://hackmd.io/@cs410/BJlyKEidMx',
       },
       rows: [
         ['Homework 1: Uninformed Search', '9/10', '9/20', [
           { label: 'Sign up sheet', href: 'https://docs.google.com/spreadsheets/d/1yRQ7LFGFHjIO5zOBgWR0DFeMewKpJ5fQsLQQNYnrDU8/edit?gid=1763574605#gid=1763574605' },
         ]],
-        ['Homework 2: Informed Search', '9/15', '9/20', '-'],
+        ['Homework 2: Informed Search', '9/15', '9/20', [
+          { label: 'Sign up sheet (same as HW1)', href: 'https://docs.google.com/spreadsheets/d/1yRQ7LFGFHjIO5zOBgWR0DFeMewKpJ5fQsLQQNYnrDU8/edit?gid=1763574605#gid=1763574605' },
+        ]],
         ['Homework 3: Adversarial Search', '9/22', '9/28', '-'],
         ['Homework 4: SAT', '9/29', '10/5', '-'],
         ['Homework 5: Constrained Optimization', '10/13', '10/19', '-'],
